fbd0f1a4502d481f00dda908f14160cbfc617d94
yilmazgunalp <yilmazgunalp@gmail.com>
2022-01-24 23:06:56 +1100

    add semicolon lint rule

--- src/components/3D/Resizable.tsx
+++ src/components/3D/Resizable.tsx
@@ -1,50 +1,51 @@
-import { Box, Flex } from '@chakra-ui/layout'
-import * as React from 'react'
-import { useRef, useState } from 'react'
+import { Box, Flex } from '@chakra-ui/layout';
+import * as React from 'react';
+import { useRef, useState } from 'react';
 
 const Resizeable = ({ children }) => {
-  const ref = useRef<HTMLDivElement>()
-  const [boxWidth, setBoxWidth] = useState(60)
+  const ref = useRef<HTMLDivElement>();
+  const [boxWidth, setBoxWidth] = useState(60);
 
   const handler: React.MouseEventHandler<HTMLDivElement> = (
     e: React.MouseEvent
   ) => {
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
+    let a = 1;
 
     const onMouseMove = (e: MouseEvent) => {
-      e.stopPropagation()
-      const offset = e.offsetX
+      e.stopPropagation();
+      const offset = e.offsetX;
       if (offset < 8) {
-        return
+        return;
       } else {
         setBoxWidth(prev => {
-          const left = offset < prev
+          const left = offset < prev;
           if (left) {
             if (prev - offset > 45) {
-              return prev - 40
+              return prev - 40;
             }
-            return prev
+            return prev;
           } else {
             if (offset - prev > 45) {
-              return prev + 40
+              return prev + 40;
             } else if (offset < 61) {
-              return 60
+              return 60;
             }
-            return prev
+            return prev;
           }
-        })
+        });
       }
-    }
+    };
     function onMouseUp() {
-      e.stopPropagation()
+      e.stopPropagation();
 
-      ref.current.removeEventListener('mousemove', onMouseMove)
-      ref.current.removeEventListener('mouseup', onMouseUp)
+      ref.current.removeEventListener('mousemove', onMouseMove);
+      ref.current.removeEventListener('mouseup', onMouseUp);
     }
-    ref.current.addEventListener('mousemove', onMouseMove)
-    ref.current.addEventListener('mouseup', onMouseUp)
-  }
+    ref.current.addEventListener('mousemove', onMouseMove);
+    ref.current.addEventListener('mouseup', onMouseUp);
+  };
 
   return (
     <Flex ref={ref} width="350px" height="30px" background="khaki">
@@ -59,6 +60,6 @@ const Resizeable = ({ children }) => {
         css={{ cursor: 'e-resize' }}
       />
     </Flex>
-  )
-}
-export default Resizeable
+  );
+};
+export default Resizeable;
